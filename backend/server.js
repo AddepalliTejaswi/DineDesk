e7b25b248af01248e85b9c76e29fdc5d7823526c
Addepalli Tejaswi <addepallitejaswi04@gmail.com>
2025-12-06 01:22:44 +0530

    backend dine desk

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = 4000;
+const DB_NAME = "mernstack"
+
+// routes
+var UserRouter = require("./routes/Users");
+var VendorRouter = require("./routes/Vendors");
+var MenuRouter = require("./routes/Menu");
+var OrderRouter = require("./routes/Orders");
+var OrderFavorite = require("./routes/Favorite");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+var path = "mongodb+srv://ratnasrijonnakuti_db_user:g4LTUPkS0BTzMGY1@cluster0.3iolp0a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+//var link = 'mongodb://127.0.0.1:27017/' + DB_NAME
+//g4LTUPkS0BTzMGY1
+
+// Connection to MongoDB
+mongoose.connect(path, { useNewUrlParser: true , useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', function () {
+    console.log("MongoDB database connection established successfully !");
+})
+
+
+// setup API endpoints
+app.use("/user", UserRouter);
+app.use("/vendor", VendorRouter);
+app.use("/menu", MenuRouter);
+app.use("/order", OrderRouter);
+app.use("/favorite", OrderFavorite);
+
+app.listen(PORT, function () {
+    console.log("Server is running on Port: " + PORT);
+});
